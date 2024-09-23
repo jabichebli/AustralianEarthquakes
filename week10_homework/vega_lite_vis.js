661dd15ebf87d82ffca37532706e3750eb398ece
Jason Abi Chebli <jabi0003@student.monash.edu>
2024-09-23 19:50:59 +1000

    task 1 html done and slight modifications to bubble plot

--- week10_homework/vega_lite_vis.js
+++ week10_homework/vega_lite_vis.js
@@ -1,19 +1,9 @@
-var vg_1 = "json/donut_plot.vg.json"; 
-vegaEmbed("#donut_plot", vg_1).then(function (result) { 
+var vg_1 = "json/bubble_plot.vg.json"; 
+vegaEmbed("#bubble_plot", vg_1).then(function (result) { 
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view 
 }).catch(console.error);
 
-var vg_2 = "json/bar_plot.vg.json"; 
-vegaEmbed("#bar_plot", vg_2).then(function (result) { 
-    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view 
-}).catch(console.error);
-
-var vg_3 = "json/dot_map.vg.json"; 
-vegaEmbed("#dot_map", vg_3).then(function (result) { 
-    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view 
-}).catch(console.error);
-
-var vg_4 = "json/show_map.vg.json"; 
-vegaEmbed("#show_map", vg_4).then(function (result) { 
+var vg_2 = "json/show_map.vg.json"; 
+vegaEmbed("#show_map", vg_2).then(function (result) { 
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view 
 }).catch(console.error);

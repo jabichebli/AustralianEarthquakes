e33d825cf04f1404a6e6ee4db74ef33f8f3b44ba
Jason Abi Chebli <jabi0003@student.monash.edu>
2024-09-24 15:54:03 +1000

    Building the webpage - half way through

--- week10_homework/vega_lite_vis.js
+++ week10_homework/vega_lite_vis.js
@@ -17,3 +17,15 @@ var vg_4 = "json/donut_plot.vg.json";
 vegaEmbed("#donut_plot", vg_4).then(function (result) { 
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view 
 }).catch(console.error);
+
+var vg_5 = "json/choropleth_plot.vg.json"; 
+vegaEmbed("#choropleth_plot", vg_5).then(function (result) { 
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view 
+}).catch(console.error);
+
+
+var vg_6 = "json/population_pyramid.vg.json"; 
+vegaEmbed("#population_pyramid", vg_6).then(function (result) { 
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view 
+}).catch(console.error);
+

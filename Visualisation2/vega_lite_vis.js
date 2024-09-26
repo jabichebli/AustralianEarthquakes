966ff30a8febd9ef31cffb1fcd6b476918d8943b
Jason Abi Chebli <jabi0003@student.monash.edu>
2024-09-26 10:13:25 +1000

    Created a copy of the data in the Visualisation 2 Folder

---
+++ Visualisation2/vega_lite_vis.js
@@ -0,0 +1,31 @@
+var vg_1 = "json/bubble_plot.vg.json"; 
+vegaEmbed("#bubble_plot", vg_1).then(function (result) { 
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view 
+}).catch(console.error);
+
+var vg_2 = "json/show_map.vg.json"; 
+vegaEmbed("#show_map", vg_2).then(function (result) { 
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view 
+}).catch(console.error);
+
+var vg_3 = "json/bar_plot.vg.json"; 
+vegaEmbed("#bar_plot", vg_3).then(function (result) { 
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view 
+}).catch(console.error);
+
+var vg_4 = "json/donut_plot.vg.json"; 
+vegaEmbed("#donut_plot", vg_4).then(function (result) { 
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view 
+}).catch(console.error);
+
+var vg_5 = "json/choropleth_plot.vg.json"; 
+vegaEmbed("#choropleth_plot", vg_5).then(function (result) { 
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view 
+}).catch(console.error);
+
+
+var vg_6 = "json/population_pyramid.vg.json"; 
+vegaEmbed("#population_pyramid", vg_6).then(function (result) { 
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view 
+}).catch(console.error);
+

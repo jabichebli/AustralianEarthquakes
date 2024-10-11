b058e5a1533c7d8b53964233bcbd89c96dd0c930
Jason Abi Chebli <jabi0003@student.monash.edu>
2024-10-11 18:03:35 +1100

    Maps nearly finalised

--- Visualisation2/vega_lite_vis.js
+++ Visualisation2/vega_lite_vis.js
@@ -8,8 +8,8 @@ vegaEmbed("#show_map", vg_2).then(function (result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view 
 }).catch(console.error);
 
-var vg_3 = "json/bar_plot.vg.json"; 
-vegaEmbed("#bar_plot", vg_3).then(function (result) { 
+var vg_3 = "json/radial_plot.vg.json"; 
+vegaEmbed("#radial_plot", vg_3).then(function (result) { 
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view 
 }).catch(console.error);
 
